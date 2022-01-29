6a8044649286ca3a4b90201c04b6d0c18d148824
Unk3wn <holzhaeuser.nico@gmail.com>
2022-01-29 17:00:46 +0100

    :sparkles: Initial Commit for the Backend

---
+++ Software Project/1 - Backend - Spring/src/main/java/de/dhbw/ase/whsikey_o_clock/WhsikeyOClockApplication.java
@@ -0,0 +1,13 @@
+package de.dhbw.ase.whsikey_o_clock;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WhsikeyOClockApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(WhsikeyOClockApplication.class, args);
+    }
+
+}
---
+++ Software Project/1 - Backend - Spring/src/test/java/de/dhbw/ase/whsikey_o_clock/WhsikeyOClockApplicationTests.java
@@ -0,0 +1,15 @@
+package de.dhbw.ase.whsikey_o_clock;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WhsikeyOClockApplicationTests {
+
+    @Test
+    void contextLoads() {
+        // Empty
+        assert(true);
+    }
+
+}
