587b4132136e62e39d6c2642d95e5524e450317a
Unk3wn <holzhaeuser.nico@gmail.com>
2022-01-30 18:49:36 +0100

    :alembic: Added further layer's for Bottle to test out Spring Boot

---
+++ Software Project/1 - Backend - Spring/src/main/java/de/dhbw/ase/whsikey_o_clock/controller/BottleController.java
@@ -0,0 +1,23 @@
+package de.dhbw.ase.whsikey_o_clock.controller;
+
+import de.dhbw.ase.whsikey_o_clock.model.Bottle;
+import de.dhbw.ase.whsikey_o_clock.service.BottleService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/bottles")
+public class BottleController{
+
+    @Autowired
+    BottleService bottleService;
+
+    @GetMapping
+    public List<Bottle> getAllBottles() {
+        return bottleService.getBottles();
+    }
+}
