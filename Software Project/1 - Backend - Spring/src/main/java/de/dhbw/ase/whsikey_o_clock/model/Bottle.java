d770f6d838764f8d0b96707412273efeef26e208
Unk3wn <holzhaeuser.nico@gmail.com>
2022-01-30 18:47:10 +0100

    :sparkles: Added Model for Bottle

---
+++ Software Project/1 - Backend - Spring/src/main/java/de/dhbw/ase/whsikey_o_clock/model/Bottle.java
@@ -0,0 +1,56 @@
+package de.dhbw.ase.whsikey_o_clock.model;
+
+import de.dhbw.ase.whsikey_o_clock.model.Listener.BottleListener;
+import lombok.*;
+import org.hibernate.annotations.GenericGenerator;
+
+import javax.persistence.*;
+import java.util.UUID;
+
+@Data
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+@Entity
+@Table(name = "bottles")
+public class Bottle {
+
+    public Bottle(String label, double price, int yearOfManufacture, Manufacturer manufacturer) {
+        this.label = label;
+        this.price = price;
+        this.yearOfManufacture = yearOfManufacture;
+        this.manufacturer = manufacturer;
+    }
+
+    @Id
+    @GeneratedValue(generator = "UUID")
+    @GenericGenerator(
+            name = "UUID",
+            strategy = "org.hibernate.id.UUIDGenerator"
+    )
+    @Column(name = "uuid", updatable = false, nullable = false)
+    private UUID uuid;
+
+    @Column(name = "label")
+    private String label;
+
+    @Column(name = "price")
+    private double price;
+
+    @Column(name = "yearOfManufacture")
+    private int yearOfManufacture;
+
+    @ManyToOne
+    @JoinColumn(name = "manufacturerID")
+    private Manufacturer manufacturer;
+
+    @Column(name = "isForSale")
+    private boolean forSale;
+
+    @Column(name = "isFavorite")
+    private boolean favorite;
+
+    @Column(name = "isUnsaleable")
+    private boolean unsaleable;
+}
