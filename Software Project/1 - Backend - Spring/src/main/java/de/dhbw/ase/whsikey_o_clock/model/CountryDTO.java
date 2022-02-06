01b52fd7e73800acdf223968342dc0cac80a1a8c
Unk3wn <holzhaeuser.nico@gmail.com>
2022-02-06 17:59:24 +0100

    :sparkles: Tests with DTO Implementation

---
+++ Software Project/1 - Backend - Spring/src/main/java/de/dhbw/ase/whsikey_o_clock/model/CountryDTO.java
@@ -0,0 +1,11 @@
+package de.dhbw.ase.whsikey_o_clock.model;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+
+@Data
+@AllArgsConstructor
+public class CountryDTO {
+    private String abbreviation;
+    private String name;
+}
