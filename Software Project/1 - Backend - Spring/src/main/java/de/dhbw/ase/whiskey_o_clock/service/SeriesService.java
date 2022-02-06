88a709d0069f60fb1a3d2c603f17a7653fd70105
Unk3wn <holzhaeuser.nico@gmail.com>
2022-02-06 20:18:33 +0100

    :sparkles: Added Series Controller + Service's

---
+++ Software Project/1 - Backend - Spring/src/main/java/de/dhbw/ase/whiskey_o_clock/service/SeriesService.java
@@ -0,0 +1,17 @@
+package de.dhbw.ase.whiskey_o_clock.service;
+
+import de.dhbw.ase.whiskey_o_clock.model.Bottle;
+
+import java.util.List;
+
+public interface SeriesService {
+
+    /** CREATE **/
+
+    /** READ **/
+
+    /** UPDATE **/
+
+    /** DELETE **/
+
+}
