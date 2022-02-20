0e8292e7ff602ec80a4949d4a005517ae6a9cca9
Unk3wn <holzhaeuser.nico@gmail.com>
2022-02-21 00:15:45 +0100

    :sparkles: Added Creation of a Series like described in an UC

---
+++ Software Project/2 - Frontend - Angular/src/app/series/create/create.component.ts
@@ -0,0 +1,58 @@
+import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
+import { FormGroup, FormControl, Validators} from '@angular/forms';
+import {SeriesService} from "../series.service";
+
+@Component({
+  selector: 'app-create',
+  templateUrl: './create.component.html',
+  styleUrls: ['./create.component.css']
+})
+export class CreateComponent implements OnInit {
+
+  form!: FormGroup;
+
+  /*------------------------------------------
+  --------------------------------------------
+  Created constructor
+  --------------------------------------------
+  --------------------------------------------*/
+  constructor(
+    public seriesService: SeriesService,
+    private router: Router
+  ) { }
+
+  /**
+   * Write code on Method
+   *
+   * @return response()
+   */
+  ngOnInit(): void {
+    this.form = new FormGroup({
+      seriesLabel: new FormControl('', [Validators.required]),
+    });
+  }
+
+  /**
+   * Write code on Method
+   *
+   * @return response()
+   */
+  get f(){
+    return this.form.controls;
+  }
+
+  /**
+   * Write code on Method
+   *
+   * @return response()
+   */
+  submit(){
+    console.log(this.form.value);
+    this.seriesService.create(this.form.value).subscribe((res:any) => {
+      console.log('Series created successfully!');
+      this.router.navigateByUrl('series/index');
+    })
+  }
+
+}
