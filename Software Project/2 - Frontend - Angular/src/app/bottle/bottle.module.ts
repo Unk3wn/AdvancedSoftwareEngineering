1bf9af2be0667c4a917d1d0c6d5de0854a5ca1b5
Unk3wn <holzhaeuser.nico@gmail.com>
2022-02-20 21:11:22 +0100

    :sparkles: Added Module for Bottles

---
+++ Software Project/2 - Frontend - Angular/src/app/bottle/bottle.module.ts
@@ -0,0 +1,14 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+import { BottleRoutingModule } from './bottle-routing.module';
+
+
+@NgModule({
+  declarations: [],
+  imports: [
+    CommonModule,
+    BottleRoutingModule
+  ]
+})
+export class BottleModule { }
