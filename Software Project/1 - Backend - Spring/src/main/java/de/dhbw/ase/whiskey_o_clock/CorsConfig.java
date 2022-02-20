e453b2335453f85b78988dff000f427ae76cae82
Unk3wn <holzhaeuser.nico@gmail.com>
2022-02-20 22:23:28 +0100

    :bug: Fixed Cors

---
+++ Software Project/1 - Backend - Spring/src/main/java/de/dhbw/ase/whiskey_o_clock/CorsConfig.java
@@ -0,0 +1,22 @@
+package de.dhbw.ase.whiskey_o_clock;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
+
+@Configuration
+class CorsConfiguration {
+
+    @Bean
+    public WebMvcConfigurer corsConfigurer() {
+        return new WebMvcConfigurerAdapter() {
+            @Override
+            public void addCorsMappings(CorsRegistry registry) {
+                registry.addMapping("/**")
+                        .allowedMethods("HEAD", "GET", "PUT", "POST", "DELETE", "PATCH");
+            }
+        };
+    }
+}
