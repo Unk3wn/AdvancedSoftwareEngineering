3d59d0c24083f7775c6e859a0326fae21e2162ba
Unk3wn <holzhaeuser.nico@gmail.com>
2022-02-20 21:08:17 +0100

    :sparkles: Added Module for getting the Country's from the Backend

---
+++ Software Project/2 - Frontend - Angular/src/app/country/ICountry.ts
@@ -0,0 +1,5 @@
+export interface ICountry {
+  uuid : string,
+  abbreviation: string,
+  name : string
+}
---
+++ Software Project/2 - Frontend - Angular/src/app/country/index/index.component.ts
@@ -0,0 +1,33 @@
+import { Component, OnInit } from '@angular/core';
+import {ICountry} from "../ICountry";
+import {CountryService} from "../country.service";
+
+@Component({
+  selector: 'app-index',
+  templateUrl: './index.component.html',
+  styleUrls: ['./index.component.css']
+})
+export class IndexComponent implements OnInit {
+
+  countrys : ICountry[] = [];
+
+  /*------------------------------------------
+ --------------------------------------------
+ Created constructor
+ --------------------------------------------
+ --------------------------------------------*/
+  constructor(public countryService: CountryService) { }
+
+  /**
+   * Write code on Method
+   *
+   * @return response()
+   */
+  ngOnInit(): void {
+    this.countryService.getAll().subscribe((data: ICountry[])=>{
+      this.countrys = data;
+      console.log(this.countrys);
+    })
+  }
+
+}
