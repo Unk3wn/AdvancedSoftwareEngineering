b7141a1bd9934c3c9fd9a4c2b7b2fcc0cc24c344
Unk3wn <holzhaeuser.nico@gmail.com>
2022-02-20 21:12:41 +0100

    :sparkles: Added a Component for View of the Bottles

--- Software Project/2 - Frontend - Angular/src/app/bottle/bottle.module.ts
+++ Software Project/2 - Frontend - Angular/src/app/bottle/bottle.module.ts
@@ -2,10 +2,13 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 
 import { BottleRoutingModule } from './bottle-routing.module';
+import { IndexComponent } from './index/index.component';
 
 
 @NgModule({
-  declarations: [],
+  declarations: [
+    IndexComponent
+  ],
   imports: [
     CommonModule,
     BottleRoutingModule
