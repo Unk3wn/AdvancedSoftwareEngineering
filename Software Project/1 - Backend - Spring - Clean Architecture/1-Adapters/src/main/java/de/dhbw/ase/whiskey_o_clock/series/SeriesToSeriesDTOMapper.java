b80de44801d949dce0a35ce89b6cf0ecaa1d71db
Unk3wn <holzhaeuser.nico@gmail.com>
2022-02-25 12:03:30 +0100

    :bug: Fixed Bug in Mapper

--- Software Project/1 - Backend - Spring - Clean Architecture/1-Adapters/src/main/java/de/dhbw/ase/whiskey_o_clock/series/SeriesToSeriesDTOMapper.java
+++ Software Project/1 - Backend - Spring - Clean Architecture/1-Adapters/src/main/java/de/dhbw/ase/whiskey_o_clock/series/SeriesToSeriesDTOMapper.java
@@ -1,20 +1,12 @@
 package de.dhbw.ase.whiskey_o_clock.series;
 
-import de.dhbw.ase.whiskey_o_clock.bottle.BottleToBottleDTOMapper;
 import de.dhbw.ase.whiskey_o_clock.domain.series.Series;
 import org.springframework.stereotype.Component;
 
 import java.util.function.Function;
-import java.util.stream.Collectors;
 
 @Component
 public class SeriesToSeriesDTOMapper implements Function<Series, SeriesDTO> {
-
-    private BottleToBottleDTOMapper bottleToBottleDTOMapper;
-
-    public SeriesToSeriesDTOMapper(BottleToBottleDTOMapper bottleToBottleDTOMapper){
-        this.bottleToBottleDTOMapper = bottleToBottleDTOMapper;
-    }
 
     @Override
     public SeriesDTO apply(Series series) {
@@ -22,7 +14,7 @@ public class SeriesToSeriesDTOMapper implements Function<Series, SeriesDTO> {
     }
 
     private SeriesDTO map(Series series) {
-        return new SeriesDTO(series.getUuid(),series.getLabel(),series.getBottleList().stream().map(bottleToBottleDTOMapper).collect(Collectors.toList()));
+        return new SeriesDTO(series.getUuid(),series.getLabel(),null);
     }
 
 }
